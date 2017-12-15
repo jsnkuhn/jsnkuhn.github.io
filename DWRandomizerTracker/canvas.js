66d6fb78919185b107250f67a32b2f050ab6ddb4
jsnkuhn <jsnkuhn@gmail.com>
2017-12-15 12:45:37 -0600

    Update canvas.js

--- DWRandomizerTracker/canvas.js
+++ DWRandomizerTracker/canvas.js
@@ -4,7 +4,6 @@ var context = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 800;
 
-console.log(window.innerHeight)
 var radius = 1;
 var dragging = false;
 var color = "#0070ec"
@@ -38,9 +37,11 @@ canvas.addEventListener('mousedown', engage);
 canvas.addEventListener('mousemove', putPoint);
 canvas.addEventListener('mouseup', disengage);
 
+document.getElementById("button-clear-canvas").addEventListener('click', clearCanvas);
+
 function clearCanvas(){
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 document.getElementById("canvas-contain").scrollTo(canvas.width/3, canvas.height/3);
-document.getElementById("button-clear-canvas").addEventListener('click', clearCanvas);
+
